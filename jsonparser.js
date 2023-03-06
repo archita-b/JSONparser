c8c9b6697e6fa6151a57363bb77cb77bd21e3df2
archita-b <archita.bhattacharyya91@gmail.com>
2023-03-06 10:10:54 +0530

    JSON parser

--- jsonparser.js
+++ jsonparser.js
@@ -11,7 +11,7 @@ const booleanParser = input => {
   }
   if (input.startsWith('false')) {
    return [false, input.slice(5)];
-}
+  }
 return null;
 }
 // console.log(booleanParser('abcdtrue'));
@@ -19,8 +19,30 @@ return null;
 const numParser = input => {
   const result = input.match(/^-?([1-9]\d*|0)(\.\d+)?([Ee][+-]?\d+)?/);
   if (result) {
-    return [result[0], input.slice(result[0].length)];
+    return [Number(result[0]), input.slice(result[0].length)];
   }
   return null;
 }
-//console.log(numParser('1e-3'));
+// console.log(numParser('1e-2'));
+
+
+const stringParser = input => {
+  if (!input.startsWith('"')) return null;
+  input = input.slice(1);
+  let result = '';
+  while (input[0] !== '"') {
+    if (input[0] === "\\" && input[1] !== "u") {
+      result += input[0];
+      input = input.slice(2);
+    // } else if (input[0] === "\\" && input[1] === '"') {
+    //   result += input[0];
+    //   input = input.slice(2);
+    // 
+    } else {
+      result += input[0];
+      input = input.slice(1);
+    }
+  }
+  return [String(result),input.slice(1)];
+  }
+console.log(stringParser('"hi\b\"t\"s1"2ddfdg'));
