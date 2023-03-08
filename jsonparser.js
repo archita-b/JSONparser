a9752d9292ce14e3546056c983c9a5537eef0b08
archita-b <archita.bhattacharyya91@gmail.com>
2023-03-08 10:11:40 +0530

    JSON parser

--- jsonparser.js
+++ jsonparser.js
@@ -39,6 +39,7 @@ const stringParser = input => {
         result += sChar[0];
         input = sChar[1];
       }
+      return null;
     } else {
       result += input[0];
       input = input.slice(1);
@@ -48,21 +49,58 @@ const stringParser = input => {
   }
 
 const specialCharParser = input => {
+  if (input[1] === "\\") {
+    return ['\\', input.slice(2)];
+  }
+  if (input[1] === "\/") {
+    return ['\/', input.slice(2)];
+  }
+  if (input[1] === "b") {
+    return ['\b', input.slice(2)];
+  }
+  if (input[1] === "f") {
+    return ['\f', input.slice(2)];
+  }
+  if (input[1] === "n") {
+    return ['\n', input.slice(2)];
+  }
+  if (input[1] === "t") {
+    return ['\t', input.slice(2)];
+  }
   if (input[1] === "r") {
-    return ["\r", input.slice(2)];
+    return ['\r', input.slice(2)];
   }
   if (input[1] === '"') {
     return ['"', input.slice(2)];
   }
   if (input[1] === "u") {
-    let hex = input.slice(2,6);
-    if (hex.match(/[\u0000-\u001F]/)) {
-      return null;
-    }
-    let char = String.fromCharCode(parseInt(hex, 16));
-    console.log(char);
-    return [char, input.slice(2,6)];
+   
+  let hex = input.slice(2,6);
+  if (!hex.match(/[0-9A-Fa-f]{4}/)) {
+    return null;
+  }
+  // if (parseInt(hex, 16) >= 0 && parseInt(hex, 16) <= 31) {
+  //   console.log((parseInt(hex, 16)));
+  //   return null;
+  // }
+  let char = String.fromCharCode(parseInt(hex, 16));
+    return [char, input.slice(6)];
   }
 }
 let str = fs.readFileSync("data.json","utf8");
 console.log(stringParser(str));
+
+//  console.log(stringParser(str)[0],stringParser(str)[0].length);
+ 
+
+// const arrayParser = input => {
+//   if (!input.startsWith("[")) {
+//     return null;
+//   }
+//   let result = [];
+//   input = input.slice(1);
+//   while (input[0] !== ']') {
+
+//   }
+// }
+// console.log(arrayParser('a[1,2]'));
